058b968d81dff8bc6df2121ddef240462a397175
rafalin0 <raphalynnemejia@gmail.com>
2024-10-18 11:57:38 +0800

    style: Adjust margin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer className="text-color-1 text-center mt-7 md:mt-20 xl:mt-40 py-[30px] px-[10px] font-bold flex flex-col items-center gap-[10px] justify-center">
-      <p>2024 Rafa Lin All rights reserved</p>
+    <footer className="text-color-1 text-center mt-7 md:mt-20 2xl:mt-40 py-[30px] px-[10px] font-bold flex flex-col items-center gap-[10px] justify-center">
+      <p>© 2024 Rafa Lin. All rights reserved.</p>
       <p className="text-3xl flex gap-[10px]">
         <AiFillInstagram />
         <AiOutlineTwitter />
