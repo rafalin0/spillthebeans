68be4689e9da046575d630acaddceca8c07b9c85
rafalin0 <raphalynnemejia@gmail.com>
2024-10-18 11:26:03 +0800

    feat: add Toaster to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,8 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-
+import { Metadata } from "next";
+import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/components/theme-provider";
-import Layout from "@/components/Layout";
-
+import Layout from "@/components/Layout"; // Using the Layout component
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Spill the Beans",
@@ -24,21 +11,22 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <Layout>{children}</Layout>
+          <Layout>
+            <Toaster />
+            {children}
+          </Layout>
         </ThemeProvider>
       </body>
     </html>
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,14 @@
 import React from "react";
-import Head from "next/head";
-
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="p-[10px]">
-      <Head>
-        <title>Spill the Beans</title>
-      </Head>
       <header>
         <Navbar />
       </header>
-      <main className="max-w-[1400px] m-auto w-100%">{children}</main>
+      <main className="max-w-[1400px] mx-auto w-full">{children}</main>
       <footer>
         <Footer />
       </footer>
