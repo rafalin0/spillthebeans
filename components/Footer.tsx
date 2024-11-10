566704918f930fde25ab428585ff21d2618629e4
rafalin0 <raphalynnemejia@gmail.com>
2024-11-11 01:08:56 +0800

    feat: add Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer className="text-color-1 text-center mt-7 md:mt-20 2xl:mt-40 py-[30px] px-[10px] font-bold flex flex-col items-center gap-[10px] justify-center">
+    <footer className="text-fg-1 text-center mt-7 md:mt-20 2xl:mt-40 py-[30px] px-[10px] font-bold flex flex-col items-center gap-[10px] justify-center">
       <p>© 2024 Rafa Lin. All rights reserved.</p>
       <p className="text-3xl flex gap-[10px]">
         <AiFillInstagram />
