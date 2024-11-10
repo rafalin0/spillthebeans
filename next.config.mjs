58f3a3ca0de4923258fb63df821fe0ea8076662c
rafalin0 <raphalynnemejia@gmail.com>
2024-11-11 07:42:30 +0800

    chore: update Next.js config to include Sanity CDN domain for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+  images: {
+    domains: ["cdn.sanity.io"], // Add Sanity's CDN domain here
+  },
+};
