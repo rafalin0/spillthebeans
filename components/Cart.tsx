f181915bf2c9c178284a95a412eefb58f4d64b98
rafalin0 <raphalynnemejia@gmail.com>
2024-11-11 01:08:06 +0800

    refactor(cart): extract CartItem into a separate component

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,54 +1,42 @@
 import React, { useRef } from "react";
 import Link from "next/link";
-import {
-  AiOutlineMinus,
-  AiOutlinePlus,
-  AiOutlineLeft,
-  AiOutlineShopping,
-} from "react-icons/ai";
+import { AiOutlineLeft, AiOutlineShopping } from "react-icons/ai";
 
 import { useStore } from "@/store/store";
-import { urlFor } from "@/sanity/lib/image";
+import CheckoutBtn from "./CheckoutBtn";
+import CartItem from "./CartItem";
 
 const Cart = () => {
   const cartRef = useRef<HTMLDivElement>(null);
-  const {
-    cart,
-    count,
-    setShowCart,
-    removeProduct,
-    addProduct,
-    clearProduct,
-    total,
-  } = useStore((state) => state);
+  const { cart, count, setShowCart, total } = useStore((state) => state);
 
   return (
     <div
-      className="w-screen bg-black bg-opacity-50 fixed right-0 top-0 z-[2000] transition-all duration-100 ease-in-out"
+      className="w-screen bg-black text-fg-1 bg-opacity-50 fixed right-0 top-0 z-[2000] transition-all duration-100 ease-in-out"
       ref={cartRef}
     >
-      <div className="w-[415px] p-4 h-screen md:w-[600px] bg-white float-right md:py-10 md:px-[10px] relative flex flex-col">
+      <div className="w-full p-4 h-screen md:w-[600px] bg-bg-4 float-right md:py-10 md:px-[10px] relative flex flex-col">
         <button
           type="button"
-          className="mt-[35px] md:mt-0 flex items-center text-lg font-medium cursor-pointer gap-0.5 ml-[10px] bg-transparent"
+          className="mt-[35px] md:mt-0 flex items-center text-lg font-medium cursor-pointer gap-0.5 ml-[10px] bg-transparent "
           onClick={() => setShowCart(false)}
         >
           <AiOutlineLeft />
           <span className="ml-[10px]">Your Cart</span>
-          <span className="ml-[10px] text-[#f02d34]">({count} items)</span>
+          <span className="ml-[10px] text-fg-5">({count} items)</span>
         </button>
 
         {cart.length < 1 && (
-          <div className="m-10 text-center text-color-8 flex flex-col place-self-center">
+          <div className="m-10 text-center text-md-3 flex flex-col place-self-center">
             <AiOutlineShopping size={150} className="m-auto" />
-            <h3 className="font-semibold text-xl">
+            <h3 className="font-semibold text-xl mb-5">
               Your shopping bag is empty
             </h3>
             <Link href="/">
               <button
                 type="button"
                 onClick={() => setShowCart(false)}
-                className="w-full max-w-[400px] py-[10px] px-3 rounded-2xl text-xl mt-[10px] uppercase bg-[#f02d34] text-white cursor-pointer transform scale-100 transition-transform duration-500 ease-in-out hover:scale-110"
+                className="w-full max-w-[400px] py-[10px] px-3 rounded-sm text-xl mt-[10px] uppercase bg-fg-3 text-bg-4 cursor-pointer transform scale-100 transition-transform duration-500 ease-in-out hover:scale-110"
               >
                 Continue Shopping
               </button>
@@ -56,73 +44,19 @@ const Cart = () => {
           </div>
         )}
 
-        <div className="mt-2.5 md:mt-[15px] overflow-auto  max-h-[70vh] py-5 px-2.5 mb-2">
+        <div className="mt-2.5 md:mt-[15px] overflow-auto max-h-[70vh] py-5 pt-2.5 pb-10 md:mb-2 gap-2">
           {cart.length >= 1 &&
-            cart.map((item) => (
-              <div
-                className="py-5 px-[5px] flex gap-6 justify-between md:p-5"
-                key={item._id}
-              >
-                <img
-                  src={urlFor(item.image).url()}
-                  className="w-auto h-[100px] md:h-[150px] rounded-xl bg-color-11 p-4"
-                />
-                <div className="w-full">
-                  <div className="w-[200px] flex-wrap gap-2.5 md:flex-nowrap md:w-auto text-[#324d67] flex justify-between">
-                    <h5 className="text-base md:text-2xl text-[#324d67] font-semibold">
-                      {item.name}
-                    </h5>
-                    <h4 className="text-base md:text-xl text-black font-medium">
-                      ${item.price}
-                    </h4>
-                  </div>
-
-                  <div className="w-[200px] mt-[30px]  md:w-auto text-[#324d67] flex justify-between md:mt-[60px]">
-                    <p className="border border-gray-200 inline-flex w-min rounded-md">
-                      <span
-                        className="text-[#f02d34] text-base py-1.5 px-3 cursor-pointer m-auto"
-                        onClick={() => removeProduct(item._id)}
-                      >
-                        <AiOutlineMinus />
-                      </span>
-                      <span className="border-x border-x-gray-200 text-base py-1.5 px-3 cursor-pointer">
-                        {item.qty}
-                      </span>
-                      <span
-                        className="text-[#31a831] text-base py-1.5 px-3 cursor-pointer m-auto"
-                        onClick={() => addProduct(item)}
-                      >
-                        <AiOutlinePlus />
-                      </span>
-                    </p>
-
-                    <button
-                      type="button"
-                      className="text-base font-code text-[#f02d34] cursor-pointer bg-transparent decoration-dotted decoration-4 hover:underline underline-offset-4 lowercase"
-                      onClick={() => clearProduct(item._id)}
-                    >
-                      remove
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
+            cart.map((item) => <CartItem product={item} key={item._id} />)}
         </div>
 
         {cart.length >= 1 && (
-          <div className="p-[30px] md:py-[20px] md:px-5 absolute bottom-3 w-full bg-white">
-            <div className="text-xl md:text-[22px] flex justify-between">
+          <div className="p-5 md:py-[20px] absolute bottom-3 right-0 w-full  gap-3 flex flex-col border-t bg-bg-4 border-mg-3">
+            <div className="text-xl flex justify-between md:justify-end gap-3">
               <h3>Subtotal:</h3>
-              <h3>${total}</h3>
+              <h3 className="font-bold md:text-2xl">₱ {total}</h3>
             </div>
-            <div className="w-[300px] m-auto md:w-[400px]">
-              <button
-                type="button"
-                className="w-full max-w-[400px] py-[10px] px-3 rounded-2xl text-xl mt-[10px] uppercase bg-[#f02d34] text-white cursor-pointer transform scale-100 transition-transform duration-500 ease-in-out hover:scale-110"
-              >
-                Pay with Stripe
-              </button>
-            </div>
+
+            <CheckoutBtn />
           </div>
         )}
       </div>
---
+++ components/CartItem.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { urlFor } from "@/sanity/lib/image";
+import { useStore } from "@/store/store";
+import { CartProduct } from "@/types/cartProduct";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const CartItem = ({ product }: { product: CartProduct }) => {
+  const { editCartProduct, clearProduct } = useStore((state) => state);
+
+  const imageUrl = urlFor(product.image).url();
+
+  const [qtyDisplay, setQtyDisplay] = useState(product.qty);
+
+  useEffect(() => {
+    setQtyDisplay(product.qty);
+  }, [product.qty]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const numericValue = e.target.value.replace(/[^0-9]/g, "");
+    setQtyDisplay(Number(numericValue));
+  };
+
+  const updateQuantity = () => {
+    const quantity = Math.min(Math.max(1, qtyDisplay), 50); // Clamp between 1 and 50
+    setQtyDisplay(quantity);
+    editCartProduct({ ...product, qty: quantity }, quantity);
+  };
+
+  return (
+    <div className="py-5 px-[5px] flex gap-3 justify-between md:p-5">
+      <div className="w-auto bg-bg-2  rounded-xl">
+        <img
+          src={imageUrl}
+          className="h-[100px] md:h-[150px] p-4 dark:dark-image"
+          alt={product.name}
+        />
+      </div>
+
+      <div className="w-3/4">
+        <div className=" flex-wrap gap-2.5 md:flex-nowrap w-auto text-fg-2 flex justify-between">
+          <h5 className="text-base md:text-2xl font-semibold">
+            {product.name}
+          </h5>
+          <h4 className="text-base md:text-xl text-fg-1 font-medium">
+            ₱{product.price}
+          </h4>
+        </div>
+
+        <div className=" mt-[20px] w-auto flex justify-between md:mt-[60px]">
+          <p className="border border-mg-3 inline-flex w-min rounded-md">
+            <span
+              className={`${
+                product.qty <= 1 ? "text-mg-3" : "text-mg-2"
+              } text-base py-1.5 px-3 cursor-pointer m-auto`}
+              onClick={() => editCartProduct({ ...product }, qtyDisplay - 1)}
+            >
+              <AiOutlineMinus />
+            </span>
+            <input
+              onChange={handleInputChange}
+              className="border-x border-x-mg-3 text-base p-1.5 cursor-pointer w-10 text-center"
+              data-min="1"
+              type="text"
+              inputMode="numeric"
+              name="quantity"
+              value={qtyDisplay}
+              onBlur={updateQuantity}
+            />
+            <span
+              className={`${
+                product.qty >= 50 ? "text-mg-3" : "text-mg-1"
+              } text-base py-1.5 px-3 cursor-pointer m-auto`}
+              onClick={() => editCartProduct({ ...product }, qtyDisplay + 1)}
+            >
+              <AiOutlinePlus />
+            </span>
+          </p>
+
+          <button
+            type="button"
+            className="text-base font-code text-mg-2 cursor-pointer bg-transparent decoration-dotted decoration-4 hover:underline underline-offset-4 lowercase"
+            onClick={() => clearProduct(product._id)}
+          >
+            remove
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
