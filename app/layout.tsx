0a1ef789451944af05d9c8ae650f76676644154f
rafalin0 <raphalynnemejia@gmail.com>
2024-11-11 01:16:22 +0800

    style: add Montserrat and Playfair Display fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/components/theme-provider";
-import Layout from "@/components/Layout"; // Using the Layout component
+import Layout from "@/components/Layout";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +16,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
