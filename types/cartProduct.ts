571198dd1b6c3b37e46661ae32a749243d1052c1
rafalin0 <raphalynnemejia@gmail.com>
2024-11-11 01:01:31 +0800

    refactor(types): enhance CartProduct type with detailed properties

--- types/cartProduct.ts
+++ types/cartProduct.ts
@@ -1,3 +1,12 @@
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { SanityDocument } from "next-sanity";
 
-export type CartProduct = SanityDocument & { qty: number };
+export interface CartProduct extends SanityDocument {
+  _id: string;
+  name: string;
+  slug: string;
+  priceId: string;
+  price: number;
+  image: SanityImageSource;
+  qty: number;
+}
