99df9ba869eb8d414c4c602ea805ebca9dd9ab83
rafalin0 <raphalynnemejia@gmail.com>
2025-11-07 21:47:02 +0800

    fix: restructure BannerButton component for improved layout

--- components/BannerButton.tsx
+++ components/BannerButton.tsx
@@ -9,11 +9,11 @@ interface BannerButtonProps {
 
 const BannerButton: React.FC<BannerButtonProps> = ({buttonText, containerClasses = "", buttonClasses=""}) => {
   return (
-    <div className={containerClasses}>
-        <button type="button" className={`${buttonClasses} flex gap-1 items-center py-[10px] px-4 cursor-pointer pr-3 pl-5 md:py-[10px] md:pr-6 md:pl-8 border-2  hover:translate-y-[-10px] hover:translate-x-[10px] transition-all duration-300 ease-in-out`}>
+    <button type="button" className={`${containerClasses} min-h-[24px]`}>
+        <div className={`${buttonClasses} flex gap-1 items-center py-[10px] px-4 cursor-pointer pr-3 pl-5 md:py-[10px] md:pr-6 md:pl-8 border-2  hover:translate-y-[-10px] hover:translate-x-[10px] transition-all duration-300 ease-in-out`}>
             {buttonText} <AiOutlineRight />
-        </button>
-    </div>
+        </div>
+    </button>
   )
 }
 
